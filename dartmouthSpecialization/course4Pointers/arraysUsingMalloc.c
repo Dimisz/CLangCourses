62fa3e1a89201096b67c2626c47347163a58eef2
Vladimir Solovyov <leilyngale@icloud.com>
2022-10-29 08:23:09 +0300

    Dynamic memory allocation for arrays

---
+++ dartmouthSpecialization/course4Pointers/arraysUsingMalloc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void){
+    int * array;
+    array = (int *) malloc(5 * sizeof(int));
+    array[0] = 3;
+    aray[1] = 44;
+    array[2] = 2;
+    *(array + 3) = 7;
+    * (array + 4) = -1;
+    free(array);
+ 
+    
+    return 0;
+}
