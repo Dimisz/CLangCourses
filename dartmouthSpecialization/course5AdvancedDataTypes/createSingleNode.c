03982149309615bd034d16f7f4b4408d0965d3ba
Vladimir Solovyov <leilyngale@icloud.com>
2022-11-15 09:51:06 +0300

    Linked to other structs

--- dartmouthSpecialization/course5AdvancedDataTypes/createSingleNode.c
+++ dartmouthSpecialization/course5AdvancedDataTypes/createSingleNode.c
@@ -7,20 +7,49 @@ struct digit
     struct digit * next;
 };
 
+struct digit * append(struct digit * end, struct digit * newDigitPtr);
 struct digit * createDigit(int);
 
 int main(void)
 {
-    struct digit * numberPtr;
-    int digitToStore = 5;
+    // struct digit * numberPtr;
+    // int digitToStore = 5;
 
-    numberPtr = createDigit(digitToStore);
+    // numberPtr = createDigit(digitToStore);
 
-    printf("We are storing the digit %d and the pointer %p at memory location %p.\n", numberPtr->num, numberPtr->next, numberPtr);
-    free(numberPtr);
+    // printf("We are storing the digit %d and the pointer %p at memory location %p.\n", numberPtr->num, numberPtr->next, numberPtr);
+    // free(numberPtr);
+    struct digit * start, * newDigitPtr, * end, * tmp;
 
+    int first = 5;
+    int second = 3;
+    int third = 7;
+
+    start = createDigit(first);
+    end = start;
+
+    newDigitPtr = createDigit(second);
+    end = append(end, newDigitPtr);
+
+    newDigitPtr = createDigit(third);
+    end = append(end, newDigitPtr);
+
+    tmp = start->next;
+    free(start);
+    start = tmp;
+    tmp = start->next;
+    free(start);
+    free(tmp);
+    
     return 0;
 
+}
+
+struct digit * append(struct digit * end, struct digit * newDigitPtr)
+{
+    end->next = newDigitPtr;
+    end = newDigitPtr;
+    return end;
 }
 
 struct digit * createDigit(int dig)
